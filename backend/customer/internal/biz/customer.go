210ba1ffa2f19b50498816b41c30cf0bf40f8057
zero_01 <lyl754483458@gmail.com>
2023-12-06 22:34:17 +0800

    mysql start

---
+++ backend/customer/internal/biz/customer.go
@@ -0,0 +1,31 @@
+package biz
+
+import (
+	"database/sql"
+	"gorm.io/gorm"
+)
+
+// 业务逻辑部分
+type CustomerWork struct {
+	Telephone string `gorm:"type:varchar(15);uniqueIndex;" json:"telephone"`
+	Name      string `gorm:"type:varchar(255);uniqueIndex;" json:"name"`
+	Email     string `gorm:"type:varchar(255);uniqueIndex;" json:"email"`
+	Wechat    string `gorm:"type:varchar(255);uniqueIndex;" json:"wechat"`
+	//City   uint   `gorm:"type:Int;" json:"city"`
+}
+
+// Token部分
+type CustomerToken struct {
+	Token          string       `gorm:"type:varchar(4095);" json:"token"`
+	TokenCreatedAt sql.NullTime `gorm:"" json:"token_created_at"`
+}
+
+// 基础字段部分
+type Customer struct {
+	//利用gorm嵌入基本的四个字段
+	gorm.Model
+	//我们需要的业务逻辑
+	CustomerWork
+	//Token部分
+	CustomerToken
+}
--- backend/customer/internal/data/data.go
+++ backend/customer/internal/data/data.go
@@ -1,13 +1,14 @@
 package data
 
 import (
+	"customer/internal/biz"
 	"customer/internal/conf"
 	"fmt"
-	"github.com/redis/go-redis/v9"
-	"gorm.io/gorm"
-
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
+	"github.com/redis/go-redis/v9"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 // ProviderSet is data providers.
@@ -32,7 +33,9 @@ func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
 	redisUrl := fmt.Sprintf("redis://%s/1?dial_timeout=%d", c.Redis.Addr, 1)
 	options, err := redis.ParseURL(redisUrl)
 	if err != nil {
+
 		data.RDB = nil
+		log.Fatal(err)
 	}
 	//new client不会立即连接 需要执行命令才会连接
 	data.RDB = redis.NewClient(options) //完成客户端创建
@@ -46,5 +49,23 @@ func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
 		log.NewHelper(logger).Info("closing the data resources")
 	}
 
+	//二，初始化MDB
+	//连接mysql，使用配置
+	dsn := c.Database.Source
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		data.MDB = nil
+		fmt.Println("mysql started failed")
+		log.Fatal(err)
+	}
+	data.MDB = db
+	//开发阶段，用func migrateTable()迁移表结构; 发布阶段表结构稳定不需要再次migrate
+	migrateTable(db)
 	return data, cleanup, nil
+}
+
+func migrateTable(db *gorm.DB) {
+	if err := db.AutoMigrate(&biz.Customer{}); err != nil {
+		log.Info("customer table migrate error")
+	}
 }
